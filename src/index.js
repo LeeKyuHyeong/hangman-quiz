96cf732edf0a9ee8d2208cedd8df6e77d3290d80
이규형 <rbgud2380@naver.com>
2022-06-26 22:31:55 +0900

    Add Block.jsx

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Blocks from './chapter_15/Blocks';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter_04/clock';
-import CommentList from './chapter_05/CommentList';
-import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
-import LandingPage from './chapter_09/LandingPage';
-import AttendanceBook from './chapter_10/AttendanceBook';
-import SignUp from './chapter_11/signup';
-import Calculator from './chapter_12/Calculator';
-import ProfileCard from './chapter_13/ProfileCard';
-import DarkOrLight from './chapter_14/DarkOrLight';
-
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(
-    <React.StrictMode>
-      {/* <Library /> 
-      <Clock />
-      
-      <Accommodate />
-      <ConfirmButton />
-      <LandingPage />
-      <AttendanceBook />
-      <SignUp />
-      <Calculator />*/}
-      <CommentList />
-      <ProfileCard />
-      <DarkOrLight />
+    <React.StrictMode>    
+      <Blocks />
     </React.StrictMode>
   );
 
