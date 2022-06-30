916141f73d59908528c0bb707bd0ee8a33a93e4d
이규형 <rbgud2380@naver.com>
2022-07-01 00:23:41 +0900

    setErrorCnt function

--- game/hangman/src/component/GameBoard.js
+++ game/hangman/src/component/GameBoard.js
@@ -5,13 +5,21 @@ import { useState } from 'react';
 
 export default function ({ secretWord }) {
   const [guessedLetters, setGuessedLetters] = useState([]);
+  const [errorCnt, setErrorCnt] = useState(0);
+
   const letterGuessedHandler = (letter) => {
     let val = letter.toLowerCase();
     setGuessedLetters(prev => [...prev, val]);
+
+    if (secretWord.toLowerCase().indexOf(val) === -1) {
+      setErrorCnt(prev => prev + 1);
+    }
+
   }
   return (
     <div>
-      <LetterGrid secretWord={secretWord} guessedLetters={guessedLetters} />
+      <LetterGrid secretWord={secretWord} guessedLetters={guessedLetters}//{['a','r']}
+      />
       <ButtonGrid letterGuessed={letterGuessedHandler} />
     </div>
   );
