b5df73b2a711175516f5f717a2af6cf29db59779
이규형 <rbgud2380@naver.com>
2022-07-02 16:20:35 +0900

    divide component

--- react-project/test/src/App.js
+++ react-project/test/src/App.js
@@ -1,50 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import Myheader from './Myheader';
+import Mynav from './Mynav';
+import Mysection from './Mysection';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <MyHeader title="React" desc="Single Page Application" />
-        <MyNav />
-        <MySection title="HTML" desc="Hypertext Markup Language" />
+        <Myheader title="React" desc="Single Page Application" />
+        <Mynav />
+        <Mysection title="HTML" desc="Hypertext Markup Language" />
       </div>
     );
   }
 }
-class MyHeader extends Component {
-  render() {
-    return (
-      <header>
-        <h1 className="logo">{this.props.title}</h1>
-        <p>{this.props.desc}</p>
-      </header>
-    );
-  }
-}
 
-class MyNav extends Component {
-  render() {
-    return (
-      <ul>
-        <li onClick={() => { }}>HTML</li>
-        <li>CSS</li>
-        <li>Javascript</li>
-      </ul>
-    );
-  }
-}
 
-class MySection extends Component {
-  render() {
-    return (
-      <section>
-        <article>
-          <h2>{this.props.title}</h2>
-          <p>{this.props.desc}</p>
-        </article>
-      </section>
-    );
-  }
-}
+
+
---
+++ react-project/test/src/Mynav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li onClick={() => { }}>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+        </ul>
+      </nav>
+    );
+  }
+}
