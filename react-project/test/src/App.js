dd6bf78a3729c04d8729364369b24bd1268f561f
이규형 <rbgud2380@naver.com>
2022-07-02 15:44:03 +0900

    class component use

--- react-project/test/src/App.js
+++ react-project/test/src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Hello React!</h2>
+      <div className='App'>
+        <MyHeader />
+        <MyNav />
+        <MySection />
       </div>
     );
   }
 }
+class MyHeader extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className="logo">React</h1>
+        <p>Single Page Application</p>
+      </header>
+    );
+  }
+}
 
-export default App;
+class MyNav extends Component {
+  render() {
+    return (
+      <ul>
+        <li><a href="">HTML</a></li>
+        <li><a href="">CSS</a></li>
+        <li><a href="">Javascript</a></li>
+      </ul>
+    );
+  }
+}
+
+class MySection extends Component {
+  render() {
+    return (
+      <section>
+        <article>
+          <h2>HTML</h2>
+          <p>Hypertext Markup Language</p>
+        </article>
+      </section>
+    );
+  }
+}
