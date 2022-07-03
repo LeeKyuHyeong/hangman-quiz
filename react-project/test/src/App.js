d1b79d94ea63effa6cbe57351cea8751e70f0e94
이규형 <rbgud2380@naver.com>
2022-07-03 20:07:28 +0900

    refactroying Mynav

--- react-project/test/src/App.js
+++ react-project/test/src/App.js
@@ -5,11 +5,41 @@ import Mysection from './Mysection';
 import './App.css';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'React', desc: 'Single Page Application' },
+      menus: [
+        {
+          id: 1,
+          title: 'HTML',
+          desc: 'Hypertext Markup Language'
+        },
+        {
+          id: 2,
+          title: 'CSS',
+          desc: 'Cascade Style Sheet'
+        },
+        {
+          id: 3,
+          title: 'Javascript',
+          desc: 'Javascript is for interactive'
+        },
+        {
+          id: 4,
+          title: 'react',
+          desc: 'component state props'
+        }
+
+      ]
+    }
+  }
   render() {
     return (
       <div className='App'>
-        <Myheader title="React" desc="Single Page Application" />
-        <Mynav />
+        <Myheader title={this.state.subject.title} desc={this.state.subject.desc} />
+        <Mynav data={this.state.menus} />
         <Mysection title="HTML" desc="Hypertext Markup Language" />
       </div>
     );
--- react-project/test/src/Mynav.js
+++ react-project/test/src/Mynav.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 export default class Mynav extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data; //리팩토링
+    var i = 0;
+    while (i < data.length) {
+      lists.push("<li>" + data[i].title + "</li>");
+      console.log(lists);
+      i++;
+    }
     return (
       <nav>
         <ul>
-          <li onClick={() => { }}>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
+          {lists}
         </ul>
       </nav>
     );
