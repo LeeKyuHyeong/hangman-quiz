708165075ef27e2e6130e8144644818b8092b70d
이규형 <rbgud2380@naver.com>
2022-07-08 00:42:09 +0900

    quizTbl

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <table className="quizTbl">
+        <tr>
+          <th>/</th>
+          <th>역사</th>
+          <th>상식</th>
+          <th>넌센스</th>
+          <th>가요</th>
+          <th>랩</th>
+        </tr>
+        <tr>
+          <td>5점</td>
+          <td><a href="">순서</a></td>
+        </tr>
+        <tr>
+          <td>4점</td>
+        </tr>
+        <tr>
+          <td>3점</td>
+        </tr>
+        <tr>
+          <td>2점</td>
+        </tr>
+        <tr>
+          <td>1점</td>
+        </tr>
+      </table>
     </div>
   );
 }
