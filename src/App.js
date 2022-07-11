76fee8a838004f9f8fb7590fb9a4140fa7c5747a
kh <rbgud2380@naver.com>
2022-07-11 13:14:59 +0900

    add keyword

--- src/App.js
+++ src/App.js
@@ -8,23 +8,28 @@ function App() {
   const quizTbl = [
     {
       id: 1,
-      title: '역사'
+      title: '역사',
+      keyword: ['역사 5점키워드','4점키워드','3점키워드','2점키워드','1점키워드',]
     },
     {
       id: 2,
-      title: '상식'
+      title: '상식',
+      keyword: ['상식 5점키워드','4점키워드','3점키워드','2점키워드','1점키워드',]
     },
     {
       id: 3,
-      title: '넌센스'
+      title: '넌센스',
+      keyword: ['넌센스 5점키워드','4점키워드','3점키워드','2점키워드','1점키워드',]
     },
     {
       id: 4,
-      title: '가요'
+      title: '가요',
+      keyword: ['가요 5점키워드','4점키워드','3점키워드','2점키워드','1점키워드',]
     },
     {
       id: 5,
-      title: '랩'
+      title: '랩',
+      keyword: ['랩 5점키워드','4점키워드','3점키워드','2점키워드','1점키워드',]
     }
   ];
   var makeTh = quizTbl.map((element, index) =>
@@ -32,7 +37,7 @@ function App() {
   );
 
   var makeTr  = score.map((element,index) =>
-    <TrMaker title={quizTbl[index].title} score={element} />
+    <TrMaker title={quizTbl[index].title} score={element} keyword={quizTbl[index].keyword} />
   );
 
   return (
