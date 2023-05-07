50838ada85a7ca93e57de19db33f55925d00414a
KHLEE <rbgud2380@naver.com>
2023-05-08 01:23:08 +0900

    230508 commit

--- game/hangman/src/component/RandomWord.js
+++ game/hangman/src/component/RandomWord.js
@@ -13,7 +13,7 @@ export default function RandomWord({ onSet, isShown }) {
       id: 2,
       cate: 'fruits',
       exam: [
-        'apple', 'orange', 'banana', 'melon', 'peach', 'watermelon', 'kiwi', 'tomato', 'grape', 'mango', 'papaya', 'coconut', 'pineapple', 'cherry', 'yuja', 'Mandarin', 'lime', 'blueberry', 'plum', 'avocado', 'pear', 'strawberry', 'guaba', 'chestnut', 'fig', 'pomegranate', 'quince', 'jujube', 'msoffice', 'hwp', 'excel', 'word', 'powerpoint', 'keyword', 'keyboard', 'headset', 'userInterface', 'logitech mx master 3s', 'logitech mx keys'
+        'apple', 'orange', 'banana', 'melon', 'peach', 'watermelon', 'kiwi', 'tomato', 'grape', 'mango', 'papaya', 'coconut', 'pineapple', 'cherry', 'yuja', 'Mandarin', 'lime', 'blueberry', 'plum', 'avocado', 'pear', 'strawberry', 'guaba', 'chestnut', 'fig', 'pomegranate', 'quince', 'jujube', 'msoffice', 'hwp', 'excel', 'word', 'powerpoint', 'keyword', 'keyboard', 'headset', 'userInterface', 'logitech mx master 3s', 'logitech mx keys', 'LG moniter'
       ]
     },
     {
